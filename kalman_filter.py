bb6b5c8686889fa118e5a9fcbc1fa99a5c562ad5
Evan Clark <evan.bock.clark@gmail.com>
2018-10-15 18:26:16 +0200

    Implemented kalman_filter update, but not working properly...

--- kalman_filter.py
+++ kalman_filter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.linalg as la
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import time
@@ -7,6 +8,7 @@ from collections import namedtuple
 
 import pdb
 
+#Update step constants
 dT = 1.0
 F = np.array([[1.0, dT ],
               [0.0, 1.0]])
@@ -15,6 +17,14 @@ def updateKinematics(x, u):
     new_x = np.dot(F, x) + np.dot(B, u)
     new_x = State(*new_x)
     return new_x
+Q = np.array([[0.01, 0   ],
+              [0,    0.01]])
+
+#Predict step constants
+H = np.array([[5.6, 0  ],
+              [0,   3.5]])
+R = np.array([[0.5, 0   ],
+              [0,   0.01]])
 
 #Create State class to keep track of state
 State = namedtuple("State", ["pos", "vel"])
@@ -22,13 +32,10 @@ State = namedtuple("State", ["pos", "vel"])
 State.getVal = lambda self, key: self.__dict__[key]
 
 #Create StateVarPlot class to plot a state variable
-StateVarPlot = namedtuple("StateVarPlot", ["axis", 'truth', 'estimate', 'uncertainty'])
+StateVarPlot = namedtuple("StateVarPlot", ["axis", 'truth', 'estimate',
+                                        'uncertainty', 'measurement', 'update'])
 
 class Train:
-
-    POS_MEASUREMENT_SIGMA = 0.5
-    VEL_MEASUREMENT_SIGMA = 0.1
-
     def __init__(self, x):
         self.x = x
 
@@ -43,14 +50,13 @@ class Sensor:
         self.train = train
 
     def getMeasurement(self):
-        #Multiply by 5.6 to convert to "volts" so we can have an interesting
-        #H matrix
-        measured_pos = 5.6 * np.random.normal(self.train.x.pos,
-                                                self.POS_MEASUREMENT_SIGMA)
-        #Multiply by 3.2 to convert to "volts" so we can have an interesting
-        #H matrix
-        measured_vel = 3.2 * np.random.normal(self.train.x.vel,
-                                                self.VEL_MEASUREMENT_SIGMA)
+        #Get "measured" position and velocity (add gaussian noise)
+        measured_pos = np.random.normal(self.train.x.pos, R[0, 0])
+        measured_vel = np.random.normal(self.train.x.vel, R[1, 1])
+
+        #Convert to "volts" using inverse H matrix
+        measured_state = State(measured_pos, measured_vel)
+        measured_state = np.dot(la.inv(H), measured_state)
 
         return State(measured_pos, measured_vel)
 
@@ -58,22 +64,28 @@ class Sensor:
 class KalmanFilter:
 
     def __init__(self, x, P):
-        self.Q = np.zeros([2, 2])
+        a = np.dot(P, np.transpose(H))
+        b = la.inv(la.multi_dot([H, P, np.transpose(H)]) + R)
+        self.K_prime = np.dot(a, b)
 
         self.x = x
         self.P = P
 
     def predict(self, u):
-        #Calculate new state estimate
-        new_x = updateKinematics(self.x, u)
-        self.x = new_x
+        #Predict new state estimate based on world model
+        self.x = updateKinematics(self.x, u)
 
-        #Calculate new covariance estimate
-        new_P = np.linalg.multi_dot(F, self.P, np.transpose(F)) + self.Q
-        self.P = new_P
+        #Predict new covariance based on world model
+        self.P = la.multi_dot([F, self.P, np.transpose(F)]) + Q
 
-    def update(self):
-        pass
+    def update(self, z):
+        #Update new state estimate based on sensor model
+        new_x = self.x + np.dot(self.K_prime, (z - np.dot(H, self.x)))
+        self.x = State(*new_x)
+
+        #Pre
+        self.P = self.P - la.multi_dot([self.K_prime, H, self.P])
+
 
     def getEstimate(self):
         return self.x, self.P
@@ -96,34 +108,36 @@ class BeliefPlotter:
         self.plots = {}
 
         ax = self.fig.add_subplot(211)
-        ax.set_xlim([0, 10])
+        ax.set_xlim([-2, 12])
         ax.set_ylim([0, 1])
         truth, = ax.plot([0], [0], "y*", markersize=15, alpha=0.5)
         estimate, = ax.plot([0], [0], "bo", alpha=0.5)
-        x_axis = np.arange(0, 10, 0.001)
-        uncertainty, = ax.plot(x_axis, [0] * len(x_axis))
-        self.plots["pos"] = StateVarPlot(ax, truth, estimate, uncertainty)
+        uncertainty, = ax.plot([0], [0])
+        measurement, = ax.plot([0], [0])
+        update, = ax.plot([0], [0])
+        self.plots["pos"] = StateVarPlot(ax, truth, estimate, uncertainty,
+                                                             measurement, update)
 
         ax = self.fig.add_subplot(212)
-        ax.set_xlim([0, 10])
+        ax.set_xlim([-1, 1])
         ax.set_ylim([0, 1])
         truth, = ax.plot([0], [0], "y*", markersize=15, alpha=0.5)
         estimate, = ax.plot([0], [0], "bo", alpha=0.5)
-        x_axis = np.arange(0, 10, 0.001)
-        uncertainty, = ax.plot(x_axis, [0] * len(x_axis))
-        self.plots["vel"] = StateVarPlot(ax, truth, estimate, uncertainty)
+        uncertainty, = ax.plot([0], [0])
+        measurement, = ax.plot([0], [0])
+        update, = ax.plot([0], [0])
+        self.plots["vel"] = StateVarPlot(ax, truth, estimate, uncertainty,
+                                                            measurement, update)
 
+    def plotBelief(self, z):
+        #Update data in plot Line objects
+        self.plotStateVariable("pos", z)
+        self.plotStateVariable("vel", z)
 
-
-    def plotBelief(self):
-        self.plotStateVariable("pos")
-        self.plotStateVariable("vel")
-
+        #Redraw the plots with the new data
         self.fig.canvas.draw()
 
-    def plotStateVariable(self, var_name):
-
-
+    def plotStateVariable(self, var_name, z):
         #Get true positions
         truth_value = self.train.x.getVal(var_name)
         #Get estimated position
@@ -132,16 +146,39 @@ class BeliefPlotter:
         cov_index = self.kalman_filter.getCovarianceIndex(var_name)
         est_variance = self.kalman_filter.P[cov_index, cov_index]
 
-        print truth_value, est_value
+        print "%s variance: %s" % (var_name, est_variance)
 
+        #Get plot Line objects for this state variable
         truth_plot = self.plots[var_name].truth
         est_plot =  self.plots[var_name].estimate
         uncertainty_plot = self.plots[var_name].uncertainty
+        measurement_plot =  self.plots[var_name].measurement
+        update_plot = self.plots[var_name].update
 
+        #Update them with the latest data
         truth_plot.set_xdata([truth_value])
         est_plot.set_xdata([est_value])
-        x_axis = np.arange(0, 10, 0.001)
-        uncertainty_plot.set_ydata(mlab.normpdf(x_axis, est_value, est_variance))
+        x = np.arange(-15, 15, 0.001)
+        y = mlab.normpdf(x, est_value, est_variance)
+        uncertainty_plot.set_xdata(x)
+        uncertainty_plot.set_ydata(y)
+        if z is None:
+            measurement_plot.set_xdata([0])
+            measurement_plot.set_ydata([0])
+            update_plot.set_xdata([0])
+            update_plot.set_ydata([0])
+        else:
+            # pdb.set_trace()
+            meas_val = z.getVal(var_name)
+            meas_cov = R[cov_index, cov_index]
+            y2 = mlab.normpdf(x, meas_val, meas_cov)
+            measurement_plot.set_xdata(x)
+            measurement_plot.set_ydata(y2)
+
+            y3 = np.multiply(y, y2)
+            y3 /= np.max(y3)
+            update_plot.set_xdata(x)
+            update_plot.set_ydata(y3)
 
 
 def main():
@@ -149,16 +186,42 @@ def main():
     train = Train(init_truth_state)
 
     init_estimate = State(0.0, .1)
-    init_covariance = np.identity(2) * 0.1
+    init_covariance = np.identity(2) * 0.01
     kalman_filter = KalmanFilter(init_estimate, init_covariance)
 
     plotter = BeliefPlotter(train, kalman_filter)
+    sensor = Sensor(train)
 
-    for i in range(500):
-        plotter.plotBelief()
+    z = None
+    u = 0.01
+    for i in range(1000):
+        if train.x.pos < 2.5:
+            u = 0.01
+        elif train.x.pos >= 2.5 and train.x.pos <= 7.5:
+            u = 0
+        else:
+            u = -0.01
 
-        train.update(0.01)
-        time.sleep(0.1)
+        #Plot latest state
+        plotter.plotBelief(z)
+        raw_input()
+
+        #Update train state and kalman filter estimate
+        train.update(u)
+        #Give the kalman filter a measurement of the train every 100 iterations
+        if i % 10 == 0:
+            z = sensor.getMeasurement()
+            plotter.plotBelief(z)
+            raw_input()            
+            kalman_filter.update(z)
+            plotter.plotBelief(z)
+            raw_input()
+        else:
+            kalman_filter.predict(u)
+            z = None
+
+        # Control animation speed
+
 
 
 
